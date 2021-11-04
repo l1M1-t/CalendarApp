b6340d6f7ac711ff5ea8f084b0f8a06ca988818a
l1M1-t <76225183+l1M1-t@users.noreply.github.com>
2021-11-03 21:50:07 -0500

    Calendarview updated

--- app/src/main/java/com/prove05/calendar/MainActivity.java
+++ app/src/main/java/com/prove05/calendar/MainActivity.java
@@ -1,14 +1,36 @@
 package com.prove05.calendar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    TextView textView;
+    CalendarView calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calendar = (CalendarView)
+                findViewById(R.id.calendar);
+        textView = (TextView)
+                findViewById(R.id.textView);
+
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(
+                    @NonNull CalendarView view, int year, int month, int dayofMonth) {
+                String date = month + "-" + dayofMonth + "-" + year;
+
+                textView.setText(date);
+            }
+        });
     }
 }
