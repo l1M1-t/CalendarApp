2b93fe6c722f295dabb5eadfc1106ad40893ef93
JavierSosa2021 <35538627+JavierSosa2021@users.noreply.github.com>
2021-11-12 19:52:30 -0300

    Thingstodo

--- app/src/main/java/com/prove05/calendar/MainActivity.java
+++ app/src/main/java/com/prove05/calendar/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
     public void dayAction(View view) {
         startActivity(new Intent(this, EventViewActivity.class));
     }
