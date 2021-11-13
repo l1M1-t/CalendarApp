f7eb539bc161436d393cba382aac46157ab55659
Fireator <fireator@live.com>
2021-11-12 18:58:03 -0700

    Holders for the storage and writing to file *version 0.1

---
+++ app/src/main/java/EventHolder.java
@@ -0,0 +1,37 @@
+public class EventHolder {
+    private String title;
+    private int date;
+    private int time;
+
+    public EventHolder(String title, int date, int time) {
+        this.title = title;
+        this.date = date;
+        this.time = time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+
+}
---
+++ app/src/main/java/StoreEvents.java
@@ -0,0 +1,50 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class StoreEvents {
+
+    public static ArrayList<EventHolder> readEventsFromFile(String fileName) throws FileNotFoundException {
+        File file = new File(fileName);
+        Scanner s = new Scanner(file);
+
+        ArrayList<EventHolder> eventHolderList = new ArrayList<EventHolder>();
+
+        while(s.hasNextLine()){
+            String line = s.nextLine();
+            String[] items = line.split("\\|");
+
+            String title = items[0];
+            int date = Integer.parseInt(items[1]);
+            int time = Integer.parseInt(items[2]);
+
+            EventHolder newEventHolder = new EventHolder(title, date, time);
+            eventHolderList.add(newEventHolder);
+
+        }
+        return eventHolderList;
+
+    }
+    public static void listToFile(ArrayList<EventHolder> eventHolderList){
+
+        for(Iterator it = eventHolderList.iterator(); it.hasNext();){
+            EventHolder currentEventHolder = (EventHolder) it.next();
+            String outputText = currentEventHolder.getTitle() + "|" + currentEventHolder.getDate() + "|" + currentEventHolder.getTime();
+        }
+
+
+    }
+
+
+
+    public static void saveToFileString(String fileName, String text, boolean append) throws IOException {
+        File file = new File(fileName);
+    }
+
+
+
+
+}
