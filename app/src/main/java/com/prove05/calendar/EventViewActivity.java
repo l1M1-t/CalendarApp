8366c697b02cc91f56807ebd8cd4a2af039af72c
l1M1-t <76225183+l1M1-t@users.noreply.github.com>
2021-11-17 20:08:26 -0600

    CreateEventActivity class properly connected with CreateEventActivity layout CreateEvent button in the MainActivity connected properly to CreateEventActivity

--- app/src/main/java/com/prove05/calendar/EventViewActivity.java
+++ app/src/main/java/com/prove05/calendar/EventViewActivity.java
@@ -3,22 +3,23 @@ package com.prove05.calendar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EventViewActivity extends AppCompatActivity{
 
+    Button monthView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_view);
+
+        monthView = (Button)findViewById(R.id.monthViewButton);
     }
 
-    public void monthAction(View view) {
+    public void monthView(View view) {
         startActivity(new Intent(this, MainActivity.class));
-    }
-
-    public void createEvent(View view) {
-
     }
 }
