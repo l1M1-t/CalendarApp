88d28819b143e8dc3d9aac76a87fd615d19f4c6b
JavierSosa2021 <35538627+JavierSosa2021@users.noreply.github.com>
2021-11-19 22:34:57 -0300

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit c1602db6808309111166dedb4b181d031a98fee9, reversing
    changes made to 04585d99caa7410860dd514b48e04340695b611f.

--- app/src/main/java/com/prove05/calendar/MainActivity.java
+++ app/src/main/java/com/prove05/calendar/MainActivity.java
@@ -7,18 +7,14 @@ import android.graphics.Color;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    CalendarView calendar;
+
     TextView textView;
-    Button createEvent;
-    Button eventList;
-    String date;
+    CalendarView calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,44 +23,18 @@ public class MainActivity extends AppCompatActivity {
 
         calendar = (CalendarView)findViewById(R.id.calendar);
         textView = (TextView)findViewById(R.id.textView);
-        createEvent = (Button)findViewById(R.id.createEventButton);
-        eventList = (Button)findViewById(R.id.eventListViewButton);
 
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(
                     @NonNull CalendarView view, int year, int month, int dayofMonth) {
-                date = month + "-" + dayofMonth + "-" + year;
-            }
-        });
+                String date = month + "-" + dayofMonth + "-" + year;
 
-        createEvent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                createEventAction(v);
-            }
-        });
-
-        eventList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                eventListAction(v);
+                textView.setText(date);
             }
         });
     }
-
-    public void eventListAction(View view) {
-        Intent intent = new Intent(MainActivity.this, EventListActivity.class);
-
-        startActivity(intent);
-    }
-
-    public void createEventAction(View view) {
-        Intent intent = new Intent(MainActivity.this, CreateEventActivity.class);
-
-        //pass the date data to the next activity
-        intent.putExtra("DATE", date);
-
-        startActivity(intent);
+    public void dayAction(View view) {
+        startActivity(new Intent(this, EventViewActivity.class));
     }
 }
